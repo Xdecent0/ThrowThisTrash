31146db8dbf4033c89530dda03901b16b189c558
Xdecent0 <xdecentwork@gmail.com>
2022-05-24 21:06:34 +0300

    Random position to trash + bug fixed

--- ThrowThisTrash/Assets/Scripts/Game_Logic/Trash_Movement.cs
+++ ThrowThisTrash/Assets/Scripts/Game_Logic/Trash_Movement.cs
@@ -4,10 +4,44 @@ using UnityEngine;
 
 public class Trash_Movement : MonoBehaviour
 {
-    [SerializeField] private float speed = 0.1f;
+    [SerializeField] float movementPerSecond = -1;
+    [SerializeField] float speed = 0.1f;
+    [SerializeField] float deadZetPosition = -14f;
 
+    float firstRoad_pos = -3f;
+    float secondRoad_pos = 0f;
+    float thirdRoad_pos = 3f;
+    float fourthRoad_pos = 6f;
+    private void Start()
+    {
+        int randomroad = Random.Range(1,5);
+        switch (randomroad)
+        {
+            case 1:
+                transform.position = new Vector3(-3f, transform.position.y, transform.position.z);
+                break;
+            case 2:
+                transform.position = new Vector3(0f, transform.position.y, transform.position.z);
+                break;
+            case 3:
+                transform.position = new Vector3(3f, transform.position.y, transform.position.z);
+                break;
+            case 4:
+                transform.position = new Vector3(6f, transform.position.y, transform.position.z);
+                break;
+        }
+    }
     private void FixedUpdate()
     {
-        transform.Translate(0, 0, -1 * speed,Space.World);
+        transform.Translate(0, 0, movementPerSecond * speed,Space.World);
     }
+    // Delete this after adding trash cans
+    private void Update()
+    {
+        if (gameObject.transform.position.z < deadZetPosition)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
