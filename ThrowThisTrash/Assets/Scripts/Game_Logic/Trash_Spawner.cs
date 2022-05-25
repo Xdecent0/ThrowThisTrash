d2d7184591bea3aadd18ccb4aefd45307a300c31
Xdecent0 <xdecentwork@gmail.com>
2022-05-25 10:40:03 +0300

    Random Spawning trash with delay

--- ThrowThisTrash/Assets/Scripts/Game_Logic/Trash_Spawner.cs
+++ ThrowThisTrash/Assets/Scripts/Game_Logic/Trash_Spawner.cs
@@ -5,4 +5,18 @@ using UnityEngine;
 public class Trash_Spawner : MonoBehaviour
 {
     [SerializeField] GameObject[] trash;
+    [SerializeField] float spawnDelay = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(TestCoroutine());
+    }
+    IEnumerator TestCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnDelay);
+            Instantiate(trash[Random.Range(0, trash.Length)]);
+        }
+    }
 }
