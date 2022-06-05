f6304d0254ad68e512e908b542318200ba686a6d
Xdecent0 <xdecentwork@gmail.com>
2022-06-05 17:02:08 +0300

    adding new stuff

---
+++ ThrowThisTrash/Assets/Scripts/Game_Logic/Trash_Swiping.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Trash_Swiping : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+   /* public Selecting selecting;
+    public Bomb_Move Bomb_Move;
+    public bool isTrash = false;
+    public bool isBomb = false;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if ((Mathf.Abs(eventData.delta.x)) > (Mathf.Abs(eventData.delta.y)))
+        {
+            if (eventData.delta.x > 0 && isTrash == true)
+            {
+                selecting.MoveRight();
+            }
+            else if (isTrash == true)
+            {
+                selecting.MoveLeft();
+            }
+            if (eventData.delta.x > 0 && isBomb == true)
+            {
+                Bomb_Move.MoveRight();
+            }
+            else if (isBomb == true)
+            {
+                Bomb_Move.MoveLeft();
+            }
+        }
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+    }
+   */
+}
